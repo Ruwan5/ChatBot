7611642895ba416a403cd5ef1d5f7b3d9c9b26e0
Ruwan <ruwannawarathna96@gmail.com>
2020-05-27 11:28:46 +0530

    Initialize ChatBot

---
+++ ChatBot/screens/LoadingScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default class LoadingScreen extends React.Component {
+    render() {
+        retrun (
+            <View>
+                <Text>Loading Screen</Text>
+            </View>
+        )
+    }
+}
