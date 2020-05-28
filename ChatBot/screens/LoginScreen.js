47aee47dc6dadc93a80998aa4739f9d0eb63225c
Ruwan <ruwannawarathna96@gmail.com>
2020-05-28 14:55:02 +0530

    input and password field created

--- ChatBot/screens/LoginScreen.js
+++ ChatBot/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, } from 'react-native';
+import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
 
 export default class LoginScreen extends React.Component {
     render() {
@@ -7,8 +7,30 @@ export default class LoginScreen extends React.Component {
             <View style={styles.container}>
                 <Text style={styles.greetings}>{'Hello!\nWellcome to ChatBot'}</Text>
 
+                <View  style={styles.errorMassage}>
+                    <Text>Error</Text>
+                </View>
+
+                <View style={styles.form}>
+                    <View>
+                        <Text style={styles.inputTitle}>Email Adress</Text>
+                        <TextInput style={styles.input} autoCapitalize="none"></TextInput>
+                    </View>
+
+                    <View style={{marginTop: 32}}>
+                        <Text style={styles.inputTitle}>Password</Text>
+                        <TextInput style={styles.input} secureTextEntry autoCapitalize="none"></TextInput>
+                    </View>
+                </View>
+
+                <TouchableOpacity style={styles.button}>
+                    <Text>Sign In</Text>
+                </TouchableOpacity>
+
             </View>
-        )
+
+
+        );
     }
 }
 
@@ -22,5 +44,36 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: "400",
         textAlign: "center"
+    },
+    errorMassage: {
+        height: 72,
+        alignItems: "center",
+        justifyContent: "center",
+        marginHorizontal: 30
+    },
+    form: {
+        marginBottom: 48,
+        marginHorizontal: 30
+    },
+    inputTitle: {
+
+        color: "#BABF9E",
+        fontSize: 10,
+        textTransform: "uppercase"
+    },
+    input: {
+        borderBottomColor: "#BABF9E",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        height: 40,
+        fontSize: 15,
+        color: "#161F3D"
+    },
+    button: {
+        marginHorizontal: 30,
+        backgroundColor: "#E9446A",
+        borderRadius: 4,
+        height: 52,
+        alignItems: "center",
+        justifyContent: "center"
     }
 })
