dc280dafe83c822356c485c2e9ea2743806235dd
Ruwan <ruwannawarathna96@gmail.com>
2020-05-28 17:19:53 +0530

    login page succesfuly

--- ChatBot/screens/LoginScreen.js
+++ ChatBot/screens/LoginScreen.js
@@ -1,32 +1,66 @@
 import React from 'react';
 import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import * as firebase from 'firebase';
 
 export default class LoginScreen extends React.Component {
+    state = {
+        email: "",
+        password: "",
+        errorMessage: null
+    }
+
+    handleLogin = () => {
+
+        const {email, password} = this.state
+
+        firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .catch(error => this.setState({errorMessage: error.message}));
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.greetings}>{'Hello!\nWellcome to ChatBot'}</Text>
 
                 <View  style={styles.errorMassage}>
-                    <Text>Error</Text>
+                    {this.state.errorMessage && <Text style={styles.error}>{this.state.errorMessage}</Text> }
                 </View>
 
                 <View style={styles.form}>
                     <View>
                         <Text style={styles.inputTitle}>Email Adress</Text>
-                        <TextInput style={styles.input} autoCapitalize="none"></TextInput>
+                        <TextInput
+                            style={styles.input}
+                            autoCapitalize="none"
+                            onChangeText={email => this.setState({ email })}
+                            value={this.state.email}>
+                        </TextInput>
                     </View>
 
                     <View style={{marginTop: 32}}>
                         <Text style={styles.inputTitle}>Password</Text>
-                        <TextInput style={styles.input} secureTextEntry autoCapitalize="none"></TextInput>
+                        <TextInput
+                            style={styles.input}
+                            secureTextEntry
+                            autoCapitalize="none"
+                            onChangeText={password => this.setState({ password})}
+                            value={this.state.password}>
+
+                        </TextInput>
                     </View>
                 </View>
 
-                <TouchableOpacity style={styles.button}>
-                    <Text>Sign In</Text>
+                <TouchableOpacity style={styles.button} onPress={this.handleLogin}>
+                    <Text style={{color: "#FFF", fontWeight: "500"}}>Sign In</Text>
                 </TouchableOpacity>
 
+                <TouchableOpacity style={{ alignSelf: "center", marginTop: 32}}>
+                    <Text style={{color: "#414959", fontSize: 13}}>
+                        New to ChatBot? <Text style={{fontWeight: "500", color: "#E9446A"}}>Sign Up</Text>
+                    </Text>
+                </TouchableOpacity>
             </View>
 
 
@@ -50,6 +84,12 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
         marginHorizontal: 30
+    },
+    error: {
+        color: "#E9446A",
+        fontSize: 13,
+        fontWeight: "600",
+        textAlign: "center"
     },
     form: {
         marginBottom: 48,
