c5b3a847483d09345727251636a7ba260f033755
Ruwan <ruwannawarathna96@gmail.com>
2020-07-26 22:25:12 +0530

    edited addPost function

--- ChatBot/screens/Firebase/Firebase.js
+++ ChatBot/screens/Firebase/Firebase.js
@@ -2,26 +2,7 @@ import firebase from "firebase";
 import firestore from '@react-native-firebase/firestore'
 
 
-// import firebaseConfig from "../../App"
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyC90VUvarpkhqlKMofe1KOm9JGAjdW5V1Y",
-//     authDomain: "chatbot-b0191.firebaseapp.com",
-//     databaseURL: "https://chatbot-b0191.firebaseio.com",
-//     projectId: "chatbot-b0191",
-//     storageBucket: "chatbot-b0191.appspot.com",
-//     messagingSenderId: "694802794380",
-//     appId: "1:694802794380:web:63cab10b7f1cc8436a38a9"
-// };
-
 class Firebase {
-    // constructor() {
-    //     if(!firebase.apps.length){
-    //         firebase.initializeApp(firebaseConfig);
-    //     }
-
-    // }
-
     addPost = async ({text, localUrl}) => {
         console.log(text)
         console.log(localUrl)
@@ -36,47 +17,26 @@ class Firebase {
                 alert("Successfully Posted!")
             )
         } else {
-            var remoteUrl = this.uploadPhotoAsync(localUrl, `photos/${this.uid}/${Date.now()}.jpg`);
-            console.log(remoteUrl)
-            firebase.firestore().collection("posts").add(
-                {text: text.toString(),
-                uid: this.uid.toString(),
-                timestamp: this.timestamp.toString(),
-                image: remoteUrl.toString()}
-            ).then(
-                alert("Successfully Posted!")
-            )
+            this.uploadPhotoAsync(localUrl, `photos/${this.uid}/${Date.now()}.jpg`).then(res => {
+                var remoteUrl = res;
+
+                firestore().collection("posts").add(
+                    {text: text.toString(),
+                    uid: this.uid.toString(),
+                    timestamp: this.timestamp.toString(),
+                    image: remoteUrl}
+                )
+                .then(
+                    alert("Successfully Posted!")
+                )
+            })
+
         }
 
-
-        // firebase.firestore().collection("posts").add({
-        //     text,
-        //     uid: this.uid,
-        //     timestamp: this.timestamp,
-        //     image: remoteUrl
-        // })
-        // return new Promise(async (res, rej) => {
-        //      firebase.firestore()
-        //         .collection("posts")
-        //         .add({
-        //             text: text,
-        //             uid: this.uid,
-        //             timestamp: this.timestamp,
-        //             image: remoteUrl
-        //         })
-        //         .then(ref => {
-        //             res(ref);
-        //         })
-        //         .catch(error => {
-        //             rej(error);
-        //         })
-        // })
     }
 
     uploadPhotoAsync = async (url, filename) => {
-        // const path = `photos/${this.uid}/${Date.now()}.jpg`
         const path = filename
-        console.log(path)
         return new Promise(async (res, rej) => {
             const response = await fetch(url)
             const file = await response.blob()
@@ -85,7 +45,13 @@ class Firebase {
 
             upload.on("state_changed", snapshot => {}, err => {
                 rej(err);
-            })
+            },
+            async () => {
+                const url = await upload.snapshot.ref.getDownloadURL();
+                console.log("test")
+                console.log(url)
+                res(url);
+            });
         })
     }
 
@@ -96,7 +62,7 @@ class Firebase {
         try{
             await firebase.auth().createUserWithEmailAndPassword(user.email, user.password);
 
-            let db = this.firestore.collection("users").doc(this.uid.toString());
+            let db = firestore().collection("users").doc(this.uid.toString());
 
             db.set({
                 fname: user.fname,
